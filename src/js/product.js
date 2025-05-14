85ccf300195217e3a50c521dbe33c47109177497
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2025-05-14 20:41:31 -0300

    Team Project W2

--- src/js/product.js
+++ src/js/product.js
@@ -1,38 +1,9 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  // We create the 'cart' variable. At this moment it is undefined.
-  let cart;  
-  try {
-    // If the local storage doesn't have any products
-    if (getLocalStorage("so-cart") === null) {
-      // we initialize the 'cart' array.
-      cart = [];
-      // Otherwise if local storage has products
-    } else {
-      // we retrieve them.
-      cart = getLocalStorage("so-cart");
-    }
-    // We add the new product into the cart array,
-    cart.push(product);
-    // and we store the updated cart back into local storage.
-    setLocalStorage("so-cart", cart);
-    // We send a message to notify the user that the product was added (temporal view).
-    alert(`✅ ${cart[0].Name} added to the cart 🛒.`);
-  } catch (error) {
-    alert("Something went wrong. Please try again later.");
-  }
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productDetail = new ProductDetails(productId, dataSource);
+productDetail.init();
