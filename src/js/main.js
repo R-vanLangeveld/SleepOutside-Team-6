adb23191d93546b5e5e3b5186f594b234da352cf
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2025-05-16 22:27:37 -0300

    Fix Dynamic and add Discount indicator-product listing

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
+console.log("productList")
 const dataSource = new ProductData("tents");
 
 const element = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, element);
+const productList = new ProductList("tents", dataSource, element);
 
+console.log("productList", "")
 productList.init();
