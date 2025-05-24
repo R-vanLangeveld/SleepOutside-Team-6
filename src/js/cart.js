66aca8296fde79515070da0a986d3daa63a36326
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2025-05-24 16:02:38 -0300

    add total to the cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,13 +4,26 @@ loadHeaderFooter();
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || "no items";
+
+
+  const cartFooter = document.querySelector(".cart-footer");
   if (cartItems === "no items") {
     document.querySelector(".product-list").innerHTML =
       "There are no items in the cart.";
+    cartFooter.classList.add("hide");
   } else {
+    cartFooter.classList.remove("hide");
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    getTotal(cartItems);
+
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
   }
+}
+
+function getTotal(cartItems) {
+  const totalCartValue = cartItems.reduce((total, item) => total + item.FinalPrice, 0);
+  const cartTotalElement = document.querySelector(".cart-total");
+  cartTotalElement.insertAdjacentHTML("beforeend", totalCartValue.toFixed(2));
 }
 
 function cartItemTemplate(item) {
