76f88175029bbc856cfa8cda3675ab43f714e962
R-vanLangeveld <rvanlangeveld@byupathway.edu>
2025-05-28 17:25:39 -0600

    Completed final team activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,14 +1,9 @@
-import {
-  getLocalStorage,
-  setLocalStorage,
-  loadHeaderFooter,
-} from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
-
   const cartFooter = document.querySelector(".cart-footer");
 
   if (cartItems.length === 0) {
@@ -19,20 +14,17 @@ function renderCartContents() {
     cartFooter.classList.remove("hide");
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     getTotal(cartItems);
-
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
   }
 }
 
 function getTotal(cartItems) {
   const totalCartValue = cartItems.reduce(
-    (total, item) => total + item.FinalPrice,
+    (total, item) => total + (item.FinalPrice * item.Qty),
     0,
   );
   const cartTotalElement = document.querySelector(".cart-total");
-
   cartTotalElement.innerHTML = "";
-
   cartTotalElement.insertAdjacentHTML(
     "beforeend",
     `Total: $${totalCartValue.toFixed(2)}`,
@@ -53,13 +45,11 @@ function cartItemTemplate(item) {
   <p class="cart-card__quantity"><span class="cart-card__remove-item" data-id="${item.Id}">X</span>Qty: ${item.Qty}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-
   return newItem;
 }
 
 function removeProductFromCart() {
   const cartList = document.querySelector(".product-list");
-
   cartList.addEventListener("click", (event) => {
     if (
       event.target &&
@@ -75,7 +65,6 @@ function removeProductFromCart() {
       if (indexElement !== -1) {
         cartUpdated.splice(indexElement, 1);
         setLocalStorage("so-cart", cartUpdated);
-
         renderCartContents();
       }
     }
